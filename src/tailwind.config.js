49371a014b803e1c2eac6f0344992afbc754012a
Nellymaina <nellymaina095@gmail.com>
2025-03-06 15:49:56 +0300

    new

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./src/**/*.{js,jsx,ts,tsx}, index.css"], // Adjusted for React
+
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", 
+      "./public/index.html"], 
     theme: {
       extend: {},
     },
